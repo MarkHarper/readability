807970a3f6ce31c9f610eeab48b4ab94e9f80383
Mark Harper <9407165+MarkHarper@users.noreply.github.com>
2022-08-13 17:12:05 -0500

    Revert "Update elixir version / deps"

--- test/readability/author_finder_test.exs
+++ test/readability/author_finder_test.exs
@@ -4,22 +4,22 @@ defmodule Readability.AuthoFinderTest do
   alias Readability.AuthorFinder
 
   test "extracting bbc format author" do
-    html = TestHelper.read_fixture("bbc.html") |> Floki.parse_document!()
+    html = TestHelper.read_fixture("bbc.html")
     assert AuthorFinder.find(html) == ["BBC News"]
   end
 
   test "extracting buzzfeed format author" do
-    html = TestHelper.read_fixture("buzzfeed.html") |> Floki.parse_document!()
+    html = TestHelper.read_fixture("buzzfeed.html")
     assert AuthorFinder.find(html) == ["Salvador Hernandez", "Hamza Shaban"]
   end
 
   test "extracting medium format author" do
-    html = TestHelper.read_fixture("medium.html") |> Floki.parse_document!()
+    html = TestHelper.read_fixture("medium.html")
     assert AuthorFinder.find(html) == ["Ken Mazaika"]
   end
 
   test "extracting nytimes format author" do
-    html = TestHelper.read_fixture("nytimes.html") |> Floki.parse_document!()
+    html = TestHelper.read_fixture("nytimes.html")
     assert AuthorFinder.find(html) == ["Judith H. Dobrzynski"]
   end
 end
--- test/readability/title_finder_test.exs
+++ test/readability/title_finder_test.exs
@@ -1,138 +1,157 @@
 defmodule Readability.TitleFinderTest do
   use ExUnit.Case, async: true
 
-  alias Readability.TitleFinder
-
   doctest Readability.TitleFinder
 
-  @html [
-    {"html", [],
-     [
-       {"head", [],
-        [
-          {"title", [], ["Tag title - test"]},
-          {"meta", [{"property", "og:title"}, {"content", "og title"}], []}
-        ]},
-       {"body", [], [{"p", [], [{"h1", [], ["h1 title"]}, {"h2", [], ["h2 title"]}]}]}
-     ]}
-  ]
+  @html """
+  <html>
+    <head>
+      <title>Tag title - test</title>
+      <meta property='og:title' content='og title'>
+    </head>
+    <body>
+      <p>
+        <h1>h1 title</h1>
+        <h2>h2 title</h2>
+      </p>
+    </body>
+  </html>
+  """
 
   test "extract most proper title" do
-    title = TitleFinder.title(@html)
+    title = Readability.TitleFinder.title(@html)
     assert title == "og title"
   end
 
   test "extract og title" do
-    title = TitleFinder.og_title(@html)
+    title = Readability.TitleFinder.og_title(@html)
     assert title == "og title"
   end
 
   test "does not merge multiple matching og:title tags" do
-    html = [
-      {"html", [],
-       [
-         {"head", [],
-          [
-            {"meta", [{"property", "og:title"}, {"content", "og title 1"}], []},
-            {"meta", [{"property", "og:title"}, {"content", "og title 2"}], []}
-          ]}
-       ]}
-    ]
+    html = """
+    <html>
+      <head>
+        <meta property='og:title' content='og title 1'>
+        <meta property='og:title' content='og title 2'>
+      </head>
+    </html>
+    """
 
-    title = TitleFinder.og_title(html)
+    title = Readability.TitleFinder.og_title(html)
     assert title == "og title 1"
   end
 
   test "extract tag title" do
-    title = TitleFinder.tag_title(@html)
+    title = Readability.TitleFinder.tag_title(@html)
     assert title == "Tag title"
 
-    html = [{"html", [], [{"head", [], [{"title", [], ["Tag title :: test"]}]}]}]
-    title = TitleFinder.tag_title(html)
+    html = """
+    <html>
+      <head>
+        <title>Tag title :: test</title>
+      </head>
+    </html>
+    """
+
+    title = Readability.TitleFinder.tag_title(html)
     assert title == "Tag title"
 
-    html = [
-      {"html", [],
-       [
-         {"head", [], [{"title", [], ["Tag title | test"]}]}
-       ]}
-    ]
+    html = """
+    <html>
+      <head>
+        <title>Tag title | test</title>
+      </head>
+    </html>
+    """
 
-    title = TitleFinder.tag_title(html)
+    title = Readability.TitleFinder.tag_title(html)
     assert title == "Tag title"
 
-    html = [
-      {"html", [],
-       [
-         {"head", [], [{"title", [], ["Tag title-tag"]}]}
-       ]}
-    ]
+    html = """
+    <html>
+      <head>
+        <title>Tag title-tag</title>
+      </head>
+    </html>
+    """
 
-    title = TitleFinder.tag_title(html)
+    title = Readability.TitleFinder.tag_title(html)
     assert title == "Tag title-tag"
 
-    html = [
-      {"html", [],
-       [
-         {"head", [], [{"title", [], ["Tag title-tag-title - test"]}]}
-       ]}
-    ]
+    html = """
+    <html>
+      <head>
+        <title>Tag title-tag-title - test</title>
+      </head>
+    </html>
+    """
 
-    title = TitleFinder.tag_title(html)
+    title = Readability.TitleFinder.tag_title(html)
     assert title == "Tag title-tag-title"
 
-    html = [
-      {"html", [],
-       [
-         {"head", [], [{"title", [], ["Tag title"]}]},
-         {"body", [], [{"svg", [], [{"title", [], ["SVG title"]}]}]}
-       ]}
-    ]
+    html = """
+    <html>
+      <head>
+        <title>Tag title</title>
+      </head>
+      <body>
+        <svg><title>SVG title</title></svg>
+      </body>
+    </html>
+    """
 
-    title = TitleFinder.tag_title(html)
+    title = Readability.TitleFinder.tag_title(html)
     assert title == "Tag title"
   end
 
   test "does not merge multiple title tags" do
-    html = [
-      {"html", [],
-       [
-         {"head", [], [{"title", [], ["tag title 1"]}, {"title", [], ["tag title 2"]}]}
-       ]}
-    ]
+    html = """
+    <html>
+      <head>
+        <title>tag title 1</title>
+        <title>tag title 2</title>
+      </head>
+    </html>
+    """
 
-    title = TitleFinder.tag_title(html)
+    title = Readability.TitleFinder.tag_title(html)
     assert title == "tag title 1"
   end
 
   test "extract h1 tag title" do
-    title = TitleFinder.h_tag_title(@html)
+    title = Readability.TitleFinder.h_tag_title(@html)
     assert title == "h1 title"
   end
 
   test "extract h2 tag title" do
-    title = TitleFinder.h_tag_title(@html, "h2")
+    title = Readability.TitleFinder.h_tag_title(@html, "h2")
     assert title == "h2 title"
   end
 
   test "does not merge multile header tags" do
-    html = [
-      {"html", [], [{"body", [], [{"h1", [], ["header 1"]}, {"h1", [], ["header 2"]}]}]}
-    ]
+    html = """
+    <html>
+      <body>
+        <h1>header 1</h1>
+        <h1>header 2</h1>
+      </body>
+    </html>
+    """
 
-    title = TitleFinder.h_tag_title(html)
+    title = Readability.TitleFinder.h_tag_title(html)
     assert title == "header 1"
   end
 
   test "returns an empty string when no title tag can be found" do
-    assert TitleFinder.tag_title([]) == ""
+    assert Readability.TitleFinder.tag_title("") == ""
   end
 
   test "returns an empty string when no og:title tag can be found" do
-    assert TitleFinder.og_title([]) == ""
+    assert Readability.TitleFinder.og_title("") == ""
   end
 
   test "returns an empty string when no header tag can be found" do
-    assert TitleFinder.h_tag_title([]) == ""
+    assert Readability.TitleFinder.h_tag_title("") == ""
   end
 end
